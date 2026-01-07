a36aa0002a29f4a53a57076c7543e0a660439654
Vivek Pasare <167676253+VivekPasare@users.noreply.github.com>
2026-01-07 22:58:48 +0530

    Time: 1 ms (57.83%), Space: 8.4 MB (99.55%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int original=x;
+        if(x<0){
+            return false;
+        }
+        else{
+            long reverse=0;
+            while(x>0){
+           
+            int digit=x % 10;
+            reverse=reverse * 10 + digit;
+            x = x/10;
+            }
+            if(original==reverse){
+                return true;
+            }
+            else{
+                return false;
+            }
+            
+            }
+        }
+    
+};
