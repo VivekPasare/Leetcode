05b0650b8ac00e52e7ac90e262ad9ce4387444be
Vivek Pasare <167676253+VivekPasare@users.noreply.github.com>
2026-01-15 17:28:09 +0530

    Time: 6 ms (49.72%), Space: 55.8 MB (36.47%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,34 +1,12 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        //sort
+        //sort array
+
         Arrays.sort(nums);
-        //frequency count
+        int mid=nums.length/2;
 
-        int count=1;
-        int ans=nums[0];
-        if(nums.length==1){
-                return ans;
+        return nums[mid];
 
-            }
-            else{
-                for(int i=1;i<nums.length;i++){
-            
-            if(nums[i-1]==nums[i]){
-                count++;
-            }
-            else{
-                count=1;
-                ans=nums[i];
-            }
 
-            if(count>nums.length/2){
-                return ans;
-            }
-
-        }
-
-            }
-        
-        return -1;
     }
 }
